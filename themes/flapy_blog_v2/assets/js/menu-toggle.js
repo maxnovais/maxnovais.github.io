48d8619b086ba8c2c6ec839d6221b53ae683f91e
Maxwell <maxnovais@gmail.com>
2014-11-11 23:48:01 -0200

    Finalização do V2

--- themes/flapy_blog_v2/assets/js/menu-toggle.js
+++ themes/flapy_blog_v2/assets/js/menu-toggle.js
@@ -1,4 +1,52 @@
-$("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
+$(window).bind('scroll', function() {
+	if ($(window).width() >= 1024){	
+		if ($(window).scrollTop() > 160) {
+			$('#top-bg').css('visibility','hidden');
+			$('#top-bg-small').css('visibility','visible');
+			$('#top-bg-line').css('visibility','hidden');
+
+		}
+		else {
+			$('#top-bg').css('visibility','visible');
+			$('#top-bg-small').css('visibility','hidden');
+			$('#top-bg-line').css('visibility','visible');
+		};
+	}
+	else{
+		$('#top-bg').css('visibility','hidden');
+		$('#top-bg-small').css('visibility','visible');
+		$('#content').css('top','70px');
+	};
+});
+
+
+$(window).resize(function(){
+	if ($(window).width() >= 1024){	
+		$('#top-bg').css('visibility','visible');
+		$('#top-bg-small').css('visibility','hidden');
+		$('#top-bg-line').css('visibility','visible');
+		$('#content').css('top','170px');
+	}
+	else{
+		$('#top-bg').css('visibility','hidden');
+		$('#top-bg-small').css('visibility','visible');
+		$('#top-bg-line').css('visibility','hidden');
+		$('#content').css('top','70px');
+	}
+});
+
+
+$(document).ready(function() {
+	if ($(window).width() >= 1024){	
+		$('#top-bg').css('visibility','visible');
+		$('#top-bg-small').css('visibility','hidden');
+		$('#top-bg-line').css('visibility','visible');
+		$('#content').css('top','170px');
+	}
+	else{
+		$('#top-bg').css('visibility','hidden');
+		$('#top-bg-small').css('visibility','visible');
+		$('#top-bg-line').css('visibility','hidden');
+		$('#content').css('top','70px');
+	}
+});
