da055d03808f8729ba8b85a5ce834f081e8f38e8
Maxwell Novais <maxwell.novais@titansgroup.com.br>
2015-01-09 14:59:02 -0200

    Fix mobile view

--- themes/flapy_blog_v2b/assets/js/animate-header.js
+++ themes/flapy_blog_v2b/assets/js/animate-header.js
@@ -5,13 +5,11 @@ $(window).scroll(function() {
         $(".avatar").addClass("avatar-shrink")
         $(".header").addClass("header-shrink")
         $(".site-title").addClass("site-title-shrink")
-        $(".fix-bar").addClass("fix-bar-shrink")
         $(".navbar-default").addClass("navbar-shrink");
     } else {
         $(".avatar").removeClass("avatar-shrink")
         $(".header").removeClass("header-shrink")
         $(".site-title").removeClass("site-title-shrink")
-        $(".fix-bar").removeClass("fix-bar-shrink")
         $(".navbar-default").removeClass("navbar-shrink");
     }
 });
